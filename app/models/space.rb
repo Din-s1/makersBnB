96bfd0f4a6ab0e22ee1638db7142d8dd8700a33c
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-10-09 14:42:33 +0100

    committing deleting a space method and tests passing

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,11 +1,9 @@
 
 class Space
-
+  CON = DBhelper.connect_to_db
   def self.all
 
-    con = DBhelper.connect_to_db
-
-    spaces = con.exec("SELECT * FROM spaces;")
+    spaces = CON.exec("SELECT * FROM spaces;")
     spaces.map do |row|
       {
         description: row['description'],
@@ -20,13 +18,11 @@ class Space
   end
 
   def self.create(description, location, price, host_id, date, guest_id)
-    con = DBhelper.connect_to_db
-    con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('#{description}','#{location}', #{price}, #{host_id}, '#{date}', #{guest_id});")
+    CON.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('#{description}','#{location}', #{price}, #{host_id}, '#{date}', #{guest_id});")
   end
 
-  def self.find_space(host_id)
-    con = DBhelper.connect_to_db
-    find = con.exec("SELECT * FROM spaces WHERE host_id = #{host_id};")
+  def self.find_spaces(host_id)
+    find = CON.exec("SELECT * FROM spaces WHERE host_id = #{host_id};")
     find.map do |row|
       {
         description: row['description'],
@@ -36,7 +32,11 @@ class Space
         date: row['date'],
         guest_id: row['guest_id']
       }
-    end 
+    end
+  end
+
+  def self.delete_space(space_id)
+    CON.exec("DELETE FROM spaces WHERE id = #{space_id}")
   end
 
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -35,7 +35,7 @@ describe Space do
     it 'finds user room based on user id' do
       con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('flat', 'Springfield', 51, 1, '06/06/06', 4);")
       con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('house', 'Drury Lane', 44, 3, '11/14/12', 6);")
-      expect(Space.find_space('1')).to eql([{
+      expect(Space.find_spaces(1)).to eql([{
               description: 'flat',
               location: 'Springfield',
               price: '$51.00',
@@ -45,4 +45,21 @@ describe Space do
             }])
     end
   end
+
+  describe '.delete_space' do
+    it 'deletes a user room based on space id' do
+      con.exec("INSERT INTO spaces(id, description, location, price, host_id, date, guest_id) VALUES (333, 'flat', 'Springfield', 51, 1, '06/06/06', 4);")
+      con.exec("INSERT INTO spaces(id, description, location, price, host_id, date, guest_id) VALUES (555, 'house', 'Drury Lane', 44, 3, '11/14/12', 6);")
+      Space.delete_space(333)
+      expect(Space.all).not_to include ({
+              description: 'flat',
+              location: 'Springfield',
+              price: '$51.00',
+              host_id: '1',
+              date: '2006-06-06',
+              guest_id: '4'
+            })
+    end
+  end
+
 end
