2836f1015d25584c474ba0eda0f56ec99bda97c4
Din Stolak <dee@Dins-MacBook-Pro.local>
2018-10-09 12:07:44 +0100

    Updated user_spec to test the creation of users. Updated USER class to create user and refactored to use a CONNECT const.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 require './spec/dbhelper.rb'
 
 class User
-
+  CONNECT = DBhelper.connect_to_db
+  def self.create(username, email, password)
+    CONNECT.exec("INSERT INTO users (username, email, password) VALUES ('#{username}', '#{email}', '#{password}')" )
+  end
 end
 
 # TODO Create user class
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require './app/models/users.rb'
+require './app/models/user.rb'
 
 describe User do
   let(:connect) { DBhelper.connect_to_db }
@@ -6,9 +6,11 @@ describe User do
   describe '.create' do
     it 'create a user' do
       User.create('Ghibli','studio@ghibli.com','Totoro')
-      expect(connect.exec('SELECT username FROM users')).to eq('Ghibli')
-      expect(connect.exec('SELECT email FROM users')).to eq('studio@ghibli.com')
-      expect(connect.exec('SELECT password FROM users')).to eq('Totoro')
+      expect(connect.exec('SELECT username, email, password FROM users')).to include({
+        'username' => 'Ghibli',
+        'email' => 'studio@ghibli.com', 
+        'password' => 'Totoro'
+      })
     end
   end
 end
