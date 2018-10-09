ca67a8d25bae228f3ecc8d89540b83acd70d1517
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-10-09 13:54:39 +0100

    committing find_space method with tests passing

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,11 +2,8 @@
 class Space
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'makersbnb_test')
-    else
-      con = PG.connect(dbname: 'makersbnb')
-    end
+
+    con = DBhelper.connect_to_db
 
     spaces = con.exec("SELECT * FROM spaces;")
     spaces.map do |row|
@@ -20,6 +17,26 @@ class Space
       }
     end
 
+  end
+
+  def self.create(description, location, price, host_id, date, guest_id)
+    con = DBhelper.connect_to_db
+    con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('#{description}','#{location}', #{price}, #{host_id}, '#{date}', #{guest_id});")
+  end
+
+  def self.find_space(host_id)
+    con = DBhelper.connect_to_db
+    find = con.exec("SELECT * FROM spaces WHERE host_id = #{host_id};")
+    find.map do |row|
+      {
+        description: row['description'],
+        location: row['location'],
+        price: row['price'],
+        host_id: row['host_id'],
+        date: row['date'],
+        guest_id: row['guest_id']
+      }
+    end 
   end
 
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -1,17 +1,23 @@
 require './app/models/space.rb'
 
 describe Space do
-
-  # describe '.create' do
-  #   it "creates a space row in the spaces db" do
-  #     space = Space.new
-  #     space.create("Springfield", "flat", 51, '06/06/06', 1, 4)
-  #   end
-  # end
+  let(:con){DBhelper.connect_to_db}
+  describe '.create' do
+    it "creates a space row in the spaces db" do
+      Space.create('flat','Springfield', 51, 1, '06/06/06', 4)
+      expect(Space.all).to include({
+              description: 'flat',
+              location: 'Springfield',
+              price: '$51.00',
+              host_id: '1',
+              date: '2006-06-06',
+              guest_id: '4'
+            })
+    end
+  end
 
   describe '.all' do
     it "retrieves all spaces in the spaces db" do
-      con = PG.connect(dbname: "makersbnb_test")
       con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('flat', 'Springfield', 51, 1, '06/06/06', 4);")
       spaces = Space.all
       expect(spaces).to include ({
@@ -22,6 +28,21 @@ describe Space do
               date: '2006-06-06',
               guest_id: '4'
             })
+    end
+  end
+
+  describe '.find_space' do
+    it 'finds user room based on user id' do
+      con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('flat', 'Springfield', 51, 1, '06/06/06', 4);")
+      con.exec("INSERT INTO spaces(description, location, price, host_id, date, guest_id) VALUES ('house', 'Drury Lane', 44, 3, '11/14/12', 6);")
+      expect(Space.find_space('1')).to eql([{
+              description: 'flat',
+              location: 'Springfield',
+              price: '$51.00',
+              host_id: '1',
+              date: '2006-06-06',
+              guest_id: '4'
+            }])
     end
   end
 end
