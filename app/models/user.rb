5c65ba81d248b6db71a373914031aa2ea0e47232
Din Stolak <dee@Dins-MacBook-Pro.local>
2018-10-09 15:15:35 +0100

    Created Email validation and username is unique and valid.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,28 @@ class User
   CONN = DBhelper.connect_to_db
 
   def self.create(username, email, password)
-    CONN.exec("INSERT INTO users (username, email, password) VALUES ('#{username}', '#{email}', '#{password}')" )
+    raise 'Email already registered!' if !CONN.exec("SELECT email FROM users WHERE email = '#{email}'").first.nil?
+    raise 'Please enter a valid email!' if  !valid_email?(email)
+    raise 'Username already registered!' if !CONN.exec("SELECT username FROM users WHERE username = '#{username}'").first.nil?
+
+    CONN.exec("INSERT INTO users (username, email, password) VALUES ('#{username}', '#{email}', '#{password}')")
   end
 
   def self.view(username)
-    CONN.exec("SELECT * FROM users WHERE username = '#{username}'")
+    res = CONN.exec("SELECT * FROM users WHERE username = '#{username}'")
+    res.map { |column|
+      {
+      id: column['id'],
+      username: column['username'],
+      email: column['email'],
+      password: column['password']
+      }
+    }
+  end
+  private
+
+  def self.valid_email?(email)
+    email =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,15 +12,30 @@ describe User do
         'password' => 'Totoro'
       })
     end
+
+
+    it "doesn't allow the same email to be entered twice" do
+      User.create('Ghibli','studio@ghibli.com','Totoro')
+      expect { User.create('Ghibli2','studio@ghibli.com','Totoro2') }.to raise_error('Email already registered!')
+    end
+
+    it "doesn't allow the same username to be entered twice" do
+      User.create('Ghibli','studio@ghibli.com','Totoro')
+      expect { User.create('Ghibli','studio2@ghibli.com','Totoro2') }.to raise_error('Username already registered!')
+    end
+
+    it 'doesnt allow an invalid email' do
+      expect { User.create('Ghibli','studioghibli.com','Totoro2') }.to raise_error('Please enter a valid email!')
+    end
   end
 
   describe '.view' do
     it 'shows all users' do
       connect.exec("INSERT INTO users (username, email, password) VALUES ('Ghibli','studio@ghibli.com','Totoro')")
       expect(User.view('Ghibli')[0]).to include({
-        'username' => 'Ghibli',
-        'email' => 'studio@ghibli.com', 
-        'password' => 'Totoro'
+        username: 'Ghibli',
+        email: 'studio@ghibli.com', 
+        password: 'Totoro'
       })
     end
   end
