4ad2a4050fbb04a8dece53f75d68e279539c2af0
Din Stolak <dee@Dins-MacBook-Pro.local>
2018-10-10 12:39:01 +0100

    created basic structure for TDD and booking class creation.

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,23 @@
 require './app/models/booking.rb'
 
 describe Booking do
-  
+
+  describe '.create' do
+    it 'Should create confirmed booking' do
+
+    end
+  end
+
+  describe '.all' do
+    it 'Should show all confirmed bookings' do
+
+    end
+  end
+
+  describe '.view' do
+    it 'Should show specific confirmed bookings' do
+      
+    end
+  end
+
 end
